6273155639d07a0b107f627ca93cec104e6348d2
darx <darxsys@gmail.com>
2014-03-27 21:31:45 +0100

    Added some more graphs to the final finis script.

--- finis-stats-complete.r
+++ finis-stats-complete.r
@@ -48,23 +48,65 @@ my_data = subset(my_data, !is.na(my_data[,10]))
 # exclude invalid gaps from the analysis
 
 pdf(paste(out_prefix, "-log-dens-real.pdf", sep=""))
-#log
-sub_log_quad = subset(my_data, my_data[,8] == "quadprog")
-sub_log_greedy = subset(my_data, my_data[,8] == "greedy" & 
+# log of size density plot
+sub_quad = subset(my_data, my_data[,8] == "quadprog")
+sub_greedy = subset(my_data, my_data[,8] == "greedy" & 
     my_data[,6] != "Invalid")
-sub_log_not = subset(my_data, my_data[,6] == "Invalid")
+sub_not = subset(my_data, my_data[,6] == "Invalid")
 
 my_cols = c("red", "blue", "coral4", "darkolivegreen1")
 my_text = c("all gaps", "quadprog", "greedy", "unsolved")
 
 plot.multi.dens(list(log10(as.numeric(my_data[,10]) + 100), 
-    log10(as.numeric(sub_log_quad[,10]) + 100), 
-    log10(as.numeric(sub_log_greedy[,10]) + 100), 
-    log10(as.numeric(sub_log_not[,10]) + 100)), 
+    log10(as.numeric(sub_quad[,10]) + 100), 
+    log10(as.numeric(sub_greedy[,10]) + 100), 
+    log10(as.numeric(sub_not[,10]) + 100)), 
     name=paste(data_name, "density of log of real gap size"), 
     my_cols, my_text,
     abline_flag=TRUE, abline_val=log10(100))
 
 dev.off()
 
+#correctness density plots - 3 plots, all, quadprog, greedy
+subvalid = subset(my_data, my_data[,6] != "Invalid")
+sub_valid_correct = subset(subvalid, subvalid[,9] == 1)
+sub_incorrect = subset(subvalid, subvalid[,9] == 0)
+sub_valid_quad = subset(subvalid, subvalid[,8] == "quadprog")
+sub_valid_quad_correct = subset(sub_valid_quad, sub_valid_quad[,9] == 1)
+sub_valid_quad_incorrect = subset(sub_valid_quad, sub_valid_quad[,9] == 0)
+sub_valid_greedy = subset(subvalid, subvalid[,8] == "greedy")
+sub_valid_greedy_correct = subset(sub_valid_greedy, sub_valid_greedy[,9] == 1)
+sub_valid_greedy_incorrect = subset(sub_valid_greedy, sub_valid_greedy[,9] == 0)
 
+my_cols = c("red", "darkolivegreen1")
+my_text = c("All correct gaps", "all wrong gaps")
+
+# all gaps
+pdf(paste(out_prefix, "-log-dens-correctness-all-real.pdf", sep=""))
+
+plot.multi.dens(list(log10(as.numeric(sub_valid_correct[,10]) + 100),
+    log10(as.numeric(sub_incorrect[,10]) + 100)), 
+    name = paste(data_name, "density plot of correctness using real gap size"), 
+    my_cols, my_text, abline_flag=TRUE, abline_val=log10(100))
+
+dev.off()
+
+my_text = c("quadprog correct gaps", "quadprog incorrect gaps") 
+pdf(paste(out_prefix, "-log-dens-correctness-quad-real.pdf", sep=""))
+
+plot.multi.dens(list(log10(as.numeric(sub_valid_quad_correct[,10]) + 100),
+    log10(as.numeric(sub_valid_quad_incorrect[,10]) + 100)), 
+    name = paste(data_name, "density plot of correctness using real gap size"), 
+    my_cols, my_text, abline_flag=TRUE, abline_val=log10(100))
+
+dev.off()
+
+my_text = c("greedy correct gaps", "greedy incorrect gaps") 
+pdf(paste(out_prefix, "-log-dens-correctness-greedy-real.pdf", sep=""))
+
+plot.multi.dens(list(log10(as.numeric(sub_valid_greedy_correct[,10]) + 100),
+    log10(as.numeric(sub_valid_greedy_incorrect[,10]) + 100)), 
+    name = paste(data_name, "density plot of correctness using real gap size"), 
+    my_cols, my_text, abline_flag=TRUE, abline_val=log10(100))
+
+dev.off()
