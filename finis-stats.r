7cba4f6c9103cf3635960f5e52469b5121b52b4c
darx <darxsys@gmail.com>
2014-04-02 22:20:49 +0200

    Fixed finis stats

--- finis-stats.r
+++ finis-stats.r
@@ -69,11 +69,15 @@ dev.off()
 sub_correct = subset(my_data, my_data[,9] == 1)
 # sub_gapcloser = NA 
 if (data_name == "Drosophila") {
+    # number of correctly closed / number of closed
     sub_gapcloser = 1976/2533
-    gapcloser_num = 1976
+    # number of closed gaps
+    gapcloser_num = 2553
 } else if (data_name == "Celegans") {
+    # number of correctly closed / number of closed
     sub_gapcloser = 8186/10802
-    gapcloser_num = 8186
+    # number of closed gaps
+    gapcloser_num = 10802
 }
 
 pdf(paste(out_prefix, "-barplot-correctness.pdf", sep=""))
@@ -117,13 +121,13 @@ smoothScatter(as.numeric(sub_quad[,6]),as.numeric(sub_quad[,5]),
 dev.off()
 
 #correctness density plots - 3 plots, all, quadprog, greedy
-subvalid = subset(my_data, my_data[,6] != "Invalid")
-sub_valid_correct = subset(subvalid, subvalid[,9] == 1)
-sub_incorrect = subset(subvalid, subvalid[,9] == 0)
-sub_valid_quad = subset(subvalid, subvalid[,8] == "quadprog")
+sub_valid = subset(my_data, my_data[,6] != "Invalid")
+sub_valid_correct = subset(sub_valid, sub_valid[,9] == 1)
+sub_valid_incorrect = subset(sub_valid, sub_valid[,9] == 0)
+sub_valid_quad = subset(sub_valid, sub_valid[,8] == "quadprog")
 sub_valid_quad_correct = subset(sub_valid_quad, sub_valid_quad[,9] == 1)
 sub_valid_quad_incorrect = subset(sub_valid_quad, sub_valid_quad[,9] == 0)
-sub_valid_greedy = subset(subvalid, subvalid[,8] == "greedy")
+sub_valid_greedy = subset(sub_valid, sub_valid[,8] == "greedy")
 sub_valid_greedy_correct = subset(sub_valid_greedy, sub_valid_greedy[,9] == 1)
 sub_valid_greedy_incorrect = subset(sub_valid_greedy, sub_valid_greedy[,9] == 0)
 
@@ -134,7 +138,7 @@ my_text = c("All correct gaps", "all wrong gaps")
 pdf(paste(out_prefix, "-log-dens-correctness-all-real.pdf", sep=""))
 
 plot.multi.dens(list(log10(as.numeric(sub_valid_correct[,10]) + 100),
-    log10(as.numeric(sub_incorrect[,10]) + 100)), 
+    log10(as.numeric(sub_valid_incorrect[,10]) + 100)), 
     name = paste(data_name, "density plot of correctness using real gap size"), 
     my_cols_two, my_text, abline_flag=TRUE, abline_val=log10(100))
 
@@ -162,32 +166,31 @@ dev.off()
 
 # barplot of number of gaps closed by finis/gapcloser
 pdf(paste(out_prefix, "-barplot-num-closed.pdf", sep=""))
-barplot(c(nrow(sub_correct), gapcloser_num,
-    nrow(sub_valid_quad_correct), nrow(sub_valid_greedy_correct)),
+barplot(c(nrow(sub_valid), gapcloser_num,
+    nrow(sub_valid_quad), nrow(sub_valid_greedy)),
     col=my_cols, 
     names.arg=c("Finis", "GapCloser", "Quadprog", "Greedy"),
     ylab="Number",
     main=paste(data_name, 
-        "barplot of correctly closed gaps of Finis and gapcloser"))
+        "barplot of closed gaps of Finis and gapcloser"))
 dev.off()
 
 
 # barplot of cathegories for each method
 size_cats = c(0, 500, 2000)
-overlaps = subset(my_data, my_data[,10] < size_cats[1])
-small_gaps = subset(my_data, 
-    my_data[,10] >= 0 & my_data[,10] < size_cats[2])
-medium_gaps = subset(my_data, 
-    my_data[,10] >= size_cats[2] & my_data[,10] < size_cats[3])
-large_gaps = subset(my_data, 
-    my_data[,10] >= size_cats[3])
+overlaps = subset(sub_valid, sub_valid[,10] < size_cats[1])
+small_gaps = subset(sub_valid, 
+    sub_valid[,10] >= size_cats[1] & sub_valid[,10] < size_cats[2])
+medium_gaps = subset(sub_valid, 
+    sub_valid[,10] >= size_cats[2] & sub_valid[,10] < size_cats[3])
+large_gaps = subset(sub_valid, 
+    sub_valid[,10] >= size_cats[3])
 
 # print(small_gaps)
 # barplot of correctness for each method
 methods = c("quadprog", "greedy")
-# my_col = c()
-# 
 leg = c("correct", "incorrect")
+
 for (m in methods) {
     m_overlaps_correct = subset(overlaps, 
         overlaps[,8] == m & overlaps[,9] == 1)
@@ -197,6 +200,10 @@ for (m in methods) {
         medium_gaps[,8] == m & medium_gaps[,9] == 1)
     m_large_gaps_correct = subset(large_gaps, 
         large_gaps[,8] == m & large_gaps[,9] == 1)
+
+    # if (m == "greedy") {
+    #     print(m_large_gaps_correct)
+    # }
 
     m_overlaps_incorrect = subset(overlaps, 
         overlaps[,8] == m & overlaps[,9] == 0)
@@ -211,13 +218,12 @@ for (m in methods) {
     name = paste(name, m, sep="")
     pdf(paste(name, "-barplot-correctness-categories.pdf", sep=""))
 
-    values = c(nrow(m_overlaps_correct), nrow(m_overlaps_incorrect),
-        nrow(m_small_gaps_correct), nrow(m_small_gaps_correct),
-        nrow(m_medium_gaps_correct), nrow(m_medium_gaps_incorrect),
-        nrow(m_large_gaps_correct), nrow(m_large_gaps_incorrect))
+    values_correct = c(nrow(m_overlaps_correct), nrow(m_small_gaps_correct), 
+        nrow(m_medium_gaps_correct), nrow(m_large_gaps_correct))
+    values_incorrect = c(nrow(m_overlaps_incorrect), nrow(m_small_gaps_incorrect), 
+        nrow(m_medium_gaps_incorrect), nrow(m_large_gaps_incorrect))
 
-    values = matrix(values, nr=2, byrow=TRUE)
-
+    x = rbind(values_correct, values_incorrect)
 
     overlap_name = paste("<", size_cats[1], sep="")
     small_name = paste(size_cats[1], "-", sep="")
@@ -226,12 +232,12 @@ for (m in methods) {
     medium_name = paste(medium_name, size_cats[3], sep="")
     large_name = paste(">=", size_cats[3], sep="")
     labels = c(overlap_name, small_name, medium_name, large_name)
-    # names(values) = labels
-    # print(labels)
-    barplot(values, col=my_cols_two, beside=TRUE, 
-        names.arg=labels, xlab = "Real gap size", 
-        ylab="Number of results",
+
+    colnames(x) = labels
+    barplot(x, beside=TRUE, col=my_cols_two, xlab = "Real gap size", 
+        ylab="Number of results", 
         main = paste(data_name, paste("barplot of correctness for", m)))
+    
     legend("topright", legend=leg, col=my_cols_two, lty=c(1,1), lwd=c(3,3))
 
     dev.off()
--- produce-graphs.sh
+++ produce-graphs.sh
@@ -2,12 +2,14 @@
 Rscript finis-stats.r ../finis/celegans/r-stats-celegans.out \
     ../finis/celegans/graphs/celegans Celegans
 sed -i 's/drosophila/celegans/g' graphs.tex
+sed -i 's/Drosophila/Celegans/g' graphs.tex
 pdflatex --output-directory=tex/ graphs.tex
 mv tex/graphs.pdf tex/celegans-graphs.pdf 
 
 Rscript finis-stats.r ../finis/drosophila/r-stats-drosophila.out \
     ../finis/drosophila/graphs/drosophila Drosophila
 sed -i 's/celegans/drosophila/g' graphs.tex
+sed -i 's/Celegans/Drosophila/g' graphs.tex
 pdflatex --output-directory=tex/ graphs.tex
 mv tex/graphs.pdf tex/drosophila-graphs.pdf 
 
